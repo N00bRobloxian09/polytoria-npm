2f936a74c8a8a9b6eaa62806ea86943a45e34d60
N00bRobloxian09 <48633944+N00bRobloxian09@users.noreply.github.com>
2021-01-20 20:14:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-const polytoria = require('polytoria-js');
 const fetch = require('axios');
+
+function user(id, thing) {
+  if(typeof(id) !== 'number') {
+    
+    const url = "https://api.polytoria.com/users/user?id=" + id;
+
+    try {
+        const response = await axios.get(url);
+        const data = response.headers['content-location'];
+        return data.thing;
+    } catch (error) {
+        throw error
+    }
+    
+  }
+  
+}
+
+module.exports = user;
