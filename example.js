d6f7d81e72d5013eed5c6cfb94efab014a7089aa
N00bRobloxian09 <48633944+N00bRobloxian09@users.noreply.github.com>
2021-05-18 20:16:35 +0200

    Add files via upload

--- example.js
+++ example.js
@@ -9,6 +9,50 @@ poly.users.getById(1).then(function (res) {
 	console.error(e);
 });
 
+// Get user's appearance
+poly.users.getAppearanceById(1).then(function (res) {
+	console.log("Appearance  HeadColor => "+ res.headBodyColor);
+	console.log("Appearance  hat1 => "+ res.hat1);
+}).catch(function (e) {
+	console.error(e);
+});
+
+// Get latest users (page, limit)
+poly.users.getLatestUsers(0, 1).then(function (res) {
+	res.data.forEach((user) => {
+		if(user == null) {
+			return;
+		}
+		console.log("latestusers => "+ user.ID);
+	});
+}).catch(function (e) {
+	console.error(e);
+});
+
+// Get user's inventory (user id, show only limiteds, limit, page)
+poly.users.getInventoryById(451, false, 1, 0).then(function (res) {
+	res.data.forEach((item) => {
+		if(item == null) {
+			return;
+		}
+		console.log("inventory => "+ item.ID);
+	});
+}).catch(function (e) {
+	console.error(e);
+});
+
+// Get user's friends -- disabled cuz spam
+/*poly.users.getFriendsById(1).then(function (res) {
+	res.data.forEach((friend) => {
+		if(friend == null) {
+			return;
+		}
+		console.log("friends => "+ friend.id);
+	});
+}).catch(function (e) {
+	console.error(e);
+});*/
+
 // Get by username and echo id
 poly.users.getByName("Polytoria").then(function (res) {
 	console.log("GetUserByUsername => "+ res.id);
